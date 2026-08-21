8a8f5b3f8258b3b2281900fe2599e32cabdaaec8
Aleksandra <a.glebocka18@gmail.com>
2026-08-21 08:03:13 +0200

    chore: add Docker and PostgreSQL setup

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+import psycopg
+import os
 
 app = FastAPI()
+
+conn = psycopg.connect(
+    host=os.getenv("DB_HOST"),
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+)
 
 
 @app.get("/")
 def read_root():
     return {"message": "Order Management API is running"}
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
