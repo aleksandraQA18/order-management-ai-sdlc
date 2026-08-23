e1add67b9c8007688265e3b78aa10e732e2a8bca
Aleksandra <a.glebocka18@gmail.com>
2026-08-23 08:20:20 +0200

    chore: set up database migrations

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,6 @@
 from uuid import UUID, uuid4
-from sqlalchemy import Integer, String
+
+from sqlalchemy import String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -10,8 +11,22 @@ class Base(DeclarativeBase):
 class Order(Base):
     __tablename__ = "orders"
 
-    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
-    customer_name: Mapped[str] = mapped_column(String, nullable=False)
-    product: Mapped[str] = mapped_column(String, nullable=False)
-    quantity: Mapped[int] = mapped_column(Integer, nullable=False)
-    status: Mapped[str] = mapped_column(String)
+    id: Mapped[UUID] = mapped_column(
+        primary_key=True,
+        default=uuid4,
+    )
+    customer_name: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+    )
+    product: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+    )
+    quantity: Mapped[int] = mapped_column(
+        nullable=False,
+    )
+    status: Mapped[str] = mapped_column(
+        String,
+        nullable=False,
+    )
